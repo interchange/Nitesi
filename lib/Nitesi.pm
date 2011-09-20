576ce786a0c159cb738cb0db5b3ded6221f5edac
Stefan Hornburg (Racke) <racke@linuxia.de>
2011-09-20 13:30:14 +0200

    Use main module as starting point instead of as Dancer application main module.

--- lib/Nitesi.pm
+++ lib/Nitesi.pm
@@ -1,10 +1,90 @@
 package Nitesi;
-use Dancer ':syntax';
 
-our $VERSION = '0.1';
+=head1 NAME
 
-get '/' => sub {
-    template 'index';
-};
+Nitesi - Open Source Shop Machine
 
-true;
+=head1 VERSION
+
+0.0001
+
+=cut
+
+our $VERSION = '0.0001';
+
+=head1 DESCRIPTION
+
+The Nitesi API is provided by the following classes:
+
+=over 4
+
+=item *
+
+L<Nitesi::Cart>
+
+=item *
+
+L<Nitesi::Account::Manager>
+
+=back
+
+=head1 CAVEATS
+
+Please anticipate API changes in this early state of development.
+
+=head1 AUTHOR
+
+Stefan Hornburg (Racke), C<racke@linuxia.de>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-nitesi at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Nitesi>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Nitesi
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Nitesi>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Nitesi>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Nitesi>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Nitesi/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2011 Stefan Hornburg (Racke).
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1;
