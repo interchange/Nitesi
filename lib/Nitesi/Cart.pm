5f397270a97dc4ee0ef3ec1e7ba065804bff7ac2
Stefan Hornburg (Racke) <racke@linuxia.de>
2011-09-28 14:23:54 +0200

    Add author and copyright to Nitesi::Cart's POD.

--- lib/Nitesi/Cart.pm
+++ lib/Nitesi/Cart.pm
@@ -297,4 +297,20 @@ sub _run_hook {
     return $ret;
 }
 
+=head1 AUTHOR
+
+Stefan Hornburg (Racke), <racke@linuxia.de>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2011 Stefan Hornburg (Racke) <racke@linuxia.de>.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+=cut
+
 1;
