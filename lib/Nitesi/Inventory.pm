8d75c9736f1203987d7e34cdf891e0b8e1086876
Stefan Hornburg (Racke) <racke@linuxia.de>
2012-05-22 15:28:49 +0200

    Add inventory role.

---
+++ lib/Nitesi/Inventory.pm
@@ -0,0 +1,28 @@
+package Nitesi::Inventory;
+
+use strict;
+use warnings;
+
+use Moo::Role;
+use Sub::Quote;
+
+=head1 NAME
+
+Nitesi::Inventory - Inventory for Nitesi Shop Machine
+
+=cut
+
+has quantity => (
+    is => 'rw',
+    lazy => 1,
+    default => quote_sub q{return 0;},
+);
+
+sub api_info {
+    return {table => 'inventory',
+	    key => 'sku',
+	    sparse => 1,
+    };
+}
+
+1;
