e8734a68990b793755b379b86deed7dcc89462a6
Stefan Hornburg (Racke) <racke@linuxia.de>
2012-08-02 16:20:13 +0200

    Add description section to Nitesi::Account::Password's POD.

--- lib/Nitesi/Account/Password.pm
+++ lib/Nitesi/Account/Password.pm
@@ -12,6 +12,11 @@ use base 'Nitesi::Object::Singleton';
 
 Nitesi::Account::Password - Password class for Nitesi Shop Machine
 
+=head1 DESCRIPTION
+
+Allows to create random passwords, password hashes from cleartext
+passwords and password checks.
+
 =head1 METHODS
 
 =head2 check
